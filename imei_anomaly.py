048b0b77c0c74a91958ae12e61b318ad870ae1f0
galihprasetyaa <107842233+galihprasetyaa@users.noreply.github.com>
2025-05-16 15:43:43 +0700

    Update imei_anomaly.py

--- imei_anomaly.py
+++ imei_anomaly.py
@@ -20,12 +20,12 @@ if uploaded_file:
 imei_counts = df["IMEI"].dropna().value_counts()
 
 # Tandai IMEI duplikat
-def mark_duplicate(imei):
+    def mark_duplicate(imei):
     if pd.isna(imei):
         return "Unknown"
     return "Duplicate" if imei_counts.get(imei, 0) > 1 else "Unique"
 
-df["IMEI_Duplicate"] = df["IMEI"].apply(mark_duplicate)
+    df["IMEI_Duplicate"] = df["IMEI"].apply(mark_duplicate)
 
 
     # --- Anomali waktu ---
